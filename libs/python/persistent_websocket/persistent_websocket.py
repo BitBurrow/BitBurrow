47e72a9f2ac7cafcddef6a139ce5cfcb4535fa16
bitinerant <bitinerant@users.noreply.github.com>
2023-09-05 10:51:49 +0100

    Update comments and variable names

--- libs/python/persistent_websocket/persistent_websocket.py
+++ libs/python/persistent_websocket/persistent_websocket.py
@@ -20,7 +20,7 @@ logger.setLevel(logging.DEBUG)  # will be throttled by handler log level (file, 
 # FIXME: scan through for 15-bit overflow in math, comparisons, etc.
 
 
-def index_otw(index):  # convert index to on-the-wire format (index mod 32768 with bit 15 set to 0)
+def lsb(index):  # convert index to on-the-wire format; see i_lsb description
     return (index % 32768).to_bytes(2, 'big')
 
 
@@ -104,15 +104,21 @@ class Timer:  # based on https://stackoverflow.com/a/45430833
 
 class PersistentWebsocket:
     # important: mirror changes in corresponding Dart code--search "bMjZmLdFv"
-    _sig_ack = 0x8010  # "I have received n total chunks" where n is next 2 bytes
-    _sig_resend = 0x8011  # "Please resend chunk n and everything after it" where n is next 2 bytes
-    _sig_resend_error = 0x8012  # "I cannot resend the requested messages"
-    ### on-the-wire format (big-endian byte order)
-    # chunk[0:2]
-    #     bit 15 → signal flag; see _sig constants
-    #     bits 0-14 → chunk index mod 32768 (first chunk sent is chunk 0) or _sig constant
-    # chunk[2:]
-    #     ack or resend index (if signal) or message (otherwise)
+    ### on-the-wire format:
+    # notes:
+    #     all bytes are in big-endian byte order; use: int.from_bytes(chunk[0:2], 'big')
+    #     index → full chunk index; first chunk sent is chunk 0
+    #     i_lsb → index mod 32768, i.e. the 15 least-significant bits
+    # a message (chunk[0:2] < 32768):
+    #     chunk[0:2]  i_lsb
+    #     chunk[2:]   message content
+    # a signal (chunk[0:2] >= 32768):
+    _sig_ack = 0x8010  # "I have received n total chunks"
+    #     chunk[2:4]  i_lsb of next expected chunk
+    _sig_resend = 0x8011  # "Please resend chunk n and everything after it"
+    #     chunk[2:4]  i_lsb of first chunk to resend
+    _sig_resend_error = 0x8012  # "I cannot resend the requested chunks"
+    #     chunk[2:]   (optional, ignored)
 
     def __init__(self):
         self._ws = None
@@ -127,7 +133,7 @@ class PersistentWebsocket:
         self._ws = ws
         logger.info(f"B17183 WebSocket connected")
         # request the other end resend what we're missing
-        await self._send_raw(const_otw(self._sig_resend) + index_otw(self._recv_index))
+        await self._send_raw(const_otw(self._sig_resend) + lsb(self._recv_index))
         try:
             # while True:
             #     chunk = await ws.receive_bytes()
@@ -158,7 +164,7 @@ class PersistentWebsocket:
                 logger.info(f"B91334 connect")
                 self._ws = ws
                 # request the other end resend what we're missing
-                await self._send_raw(const_otw(self._sig_resend) + index_otw(self._recv_index))
+                await self._send_raw(const_otw(self._sig_resend) + lsb(self._recv_index))
                 try:
                     async for chunk in ws:
                         await self.process_inbound(chunk)
@@ -174,9 +180,9 @@ class PersistentWebsocket:
 
     async def send(self, message):  # save message to resend when needed; send if we can
         if isinstance(message, str):
-            chunk = index_otw(self._journal_index) + message.encode()  # convert message to bytes
+            chunk = lsb(self._journal_index) + message.encode()  # convert message to bytes
         else:
-            chunk = index_otw(self._journal_index) + message  # message is already in bytes
+            chunk = lsb(self._journal_index) + message  # message is already in bytes
         self._journal_index += 1
         self._journal.append(chunk)
         await self._send_raw(chunk)
@@ -214,35 +220,35 @@ class PersistentWebsocket:
             logger.info(f"B44793 WebSocket disconnect")
 
     async def process_inbound(self, chunk):
-        bits0_15 = int.from_bytes(chunk[0:2], 'big')  # first 2 bytes of chunk
-        if bits0_15 < 32768:  # not a signal
-            index = unmod(bits0_15, self._recv_index, 32768)  # expand 15 bits to full index
+        i_lsb = int.from_bytes(chunk[0:2], 'big')  # first 2 bytes of chunk
+        if i_lsb < 32768:  # not a signal
+            index = unmod(i_lsb, self._recv_index, 32768)  # expand 15 bits to full index
             if index == self._recv_index:  # valid
                 self._recv_index += 1
                 # send _sig_ack after successfully receiving 16 chunks so remote can clear _journal
                 if self._recv_index - self._recv_last_ack >= 16:
-                    await self._send_raw(const_otw(self._sig_ack) + index_otw(self._recv_index))
+                    await self._send_raw(const_otw(self._sig_ack) + lsb(self._recv_index))
                     self._recv_last_ack = self._recv_index
                 return chunk[2:].decode()  # message
             elif index > self._recv_index:  # request the other end resend what we're missing
-                await self._send_raw(const_otw(self._sig_resend) + index_otw(self._recv_index))
+                await self._send_raw(const_otw(self._sig_resend) + lsb(self._recv_index))
             logger.info(f"B73822 ignoring duplicate chunk {index}")
         else:  # signal
-            if bits0_15 == self._sig_ack:
+            if i_lsb == self._sig_ack:
                 ack_index = unmod(int.from_bytes(chunk[2:4], 'big'), self._journal_index, 32768)
                 tail_index = self._journal_index - len(self._journal)
                 logger.info(f"B60966 clearing journal[{tail_index}:{ack_index}]")
                 for i in range(tail_index, ack_index):
                     self._journal.popleft()
-            elif bits0_15 == self._sig_resend:
+            elif i_lsb == self._sig_resend:
                 resend_index = unmod(int.from_bytes(chunk[2:4], 'big'), self._journal_index, 32768)
                 await self._resend(resend_index)
-            elif bits0_15 == self._sig_resend_error:
+            elif i_lsb == self._sig_resend_error:
                 await PersistentWebsocket.ensure_closed(self._ws)
                 # FIXME: need to stop trying to reconnect, pass control to outer layer
                 logger.error(f"B75561 broken connection")
             else:
-                logger.error(f"B32405 unknown signal {bits0_15}")
+                logger.error(f"B32405 unknown signal {i_lsb}")
         return None
 
     @staticmethod
