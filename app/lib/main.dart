BitBurrow/BitBurrow

commit 106d1cda7a703b93783fcb53b9228a349363104c

Removed global loginState (use singleton class instead) and logMan (does not need to be global)

app/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as storage;
 import 'dart:io' as io;
-import 'global.dart' as global;
 import 'welcome_screen.dart';
 import 'logger_manager.dart';
 import 'new_login_key_screen.dart';
@@ -16,10 +15,10 @@ import 'new_server_screen.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 final _log = Logger('main');
+var loginState = LoginState.instance;
 
 void main() async {
   usePathUrlStrategy(); // turn off the extra `#/` in the URLs
-  global.logMan = LoggerManager();
   _log.info("Begin Bitburrow app");
   WidgetsFlutterBinding.ensureInitialized();
   await onAppStart();
@@ -41,6 +40,14 @@ class LoginState {
   String get pureCoupon => coupon.replaceAll('-', ''); // for API calls
   String get pureLoginKey => loginKey.replaceAll('-', ''); // for API calls
 
+  static LoginState? _instance; // singleton--one instance for the app
+  LoginState._();
+
+  static LoginState get instance {
+    _instance ??= LoginState._();
+    return _instance!;
+  }
+
   // convert to display version used in this app, e.g. 'X88L-7V2BC-MM3P-RKVF2'
   String dressLoginKey(String pureLoginKey) {
     return '${pureLoginKey.substring(0, 4)}-${pureLoginKey.substring(4, 9)}-'
@@ -54,17 +61,17 @@ Future<void> onAppStart() async {
   // ... screen if this method takes too long
   try {
     var keyStore = const storage.FlutterSecureStorage();
-    global.loginState.hub = (await keyStore.read(key: 'hub')) ?? "";
-    _log.info("Loaded from secure storage: hub ${global.loginState.hub}");
-    global.loginState.loginKey = (await keyStore.read(key: 'login_key')) ?? "";
+    loginState.hub = (await keyStore.read(key: 'hub')) ?? "";
+    _log.info("Loaded from secure storage: hub ${loginState.hub}");
+    loginState.loginKey = (await keyStore.read(key: 'login_key')) ?? "";
     _log.info("Loaded from secure storage: "
-        "login key ${global.loginState.loginKey}");
-    global.loginState.loginKeyVerified =
+        "login key ${loginState.loginKey}");
+    loginState.loginKeyVerified =
         (await keyStore.read(key: 'login_key_verified') == 'true');
     _log.info("Loaded from secure storage: "
-        "verified status '${global.loginState.loginKeyVerified}'");
+        "verified status '${loginState.loginKeyVerified}'");
     // login key is saved if and only if it's verified and user opts to store it
-    global.loginState.saveLoginKey = global.loginState.loginKeyVerified;
+    loginState.saveLoginKey = loginState.loginKeyVerified;
   } catch (err) {
     if (io.Platform.environment['DBUS_SESSION_BUS_ADDRESS'] == 'disabled:') {
       _log.warning("B70101 D-Bus has been disabled; add this to .bashrc: "
@@ -164,8 +171,8 @@ class App extends StatelessWidget {
       }
       _log.finer("GoRouter() redirect; subloc==${state.matchedLocation}");
       if (state.matchedLocation == '/' &&
-          global.loginState.loginKeyVerified &&
-          global.loginState.skipWelcomeScreen) {
+          loginState.loginKeyVerified &&
+          loginState.skipWelcomeScreen) {
         // if we have valid login key, forward to automatic sign-in
         return '/sign-in';
       }
@@ -212,12 +219,12 @@ Widget ourScreenLayout(BuildContext context, Widget body,
                 switch (value) {
                   case "Enter a coupon code":
                     // disable 'skip' so WelcomeScreen doesn't redirect
-                    global.loginState.skipWelcomeScreen = false;
+                    loginState.skipWelcomeScreen = false;
                     context.go('/');
                     break;
                   case "Sign in":
                     // mark login key NOT verified to skip auto-sign-in
-                    global.loginState.loginKeyVerified = false;
+                    loginState.loginKeyVerified = false;
                     context.go('/sign-in');
                     break;
                   case "Servers":

app/lib/global.dart
@@ -1,5 +1,0 @@
-import 'logger_manager.dart';
-import 'main.dart';
-
-late final LoggerManager logMan;
-var loginState = LoginState();

app/lib/new_login_key_screen.dart
@@ -3,10 +3,10 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logging/logging.dart';
 import 'dart:math';
-import 'global.dart' as global;
 import 'main.dart';
 
 final _log = Logger('new_login_key_screen');
+var loginState = LoginState.instance;
 
 class NewLoginKeyScreen extends StatelessWidget {
   const NewLoginKeyScreen({Key? key}) : super(key: key);
@@ -44,7 +44,7 @@ class NewLoginKeyScreen extends StatelessWidget {
             FractionallySizedBox(
               widthFactor: 0.8,
               child: Text(
-                global.loginState.newLoginKey,
+                loginState.newLoginKey,
                 textAlign: TextAlign.center,
                 textScaleFactor: 1.8,
                 style: const TextStyle(fontWeight: FontWeight.normal),

app/lib/new_server_screen.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logging/logging.dart';
+import 'logger_manager.dart' as logm;
 import 'dart:io' as io;
 import 'dart:async' as async;
 import 'dart:convert' as convert;
 import 'dart:typed_data';
-import 'global.dart' as global;
 import 'main.dart';
 import 'parent_form_state.dart';
 import 'step_box.dart';
 import 'persistent_websocket.dart';
 
 final _log = Logger('new_server_screen');
+var loginState = LoginState.instance;
 
 class NewServerScreen extends StatelessWidget {
   const NewServerScreen({Key? key}) : super(key: key);
@@ -33,8 +34,8 @@ class NewServerForm extends ParentForm {
 
 class NewServerFormState extends ParentFormState {
   final _hubMessages = PersistentWebSocket('');
-  final hub = global.loginState.hub;
-  final lk = global.loginState.pureLoginKey;
+  final hub = loginState.hub;
+  final lk = loginState.pureLoginKey;
   async.Completer _buttonPressed = async.Completer();
   final async.Completer _hubCommanderFinished = async.Completer();
   final List<StepData> _stepsList = [];
@@ -81,11 +82,11 @@ class NewServerFormState extends ParentFormState {
   }
 
   @override
-  String getHubValue() => global.loginState.hub;
+  String getHubValue() => loginState.hub;
 
   @override
   void setHubValue(value) {
-    global.loginState.hub = value;
+    loginState.hub = value;
   }
 
   @override
@@ -93,7 +94,7 @@ class NewServerFormState extends ParentFormState {
 
   @override
   void setAccountValue(value) {
-    global.loginState.loginKey = value;
+    loginState.loginKey = value;
   }
 
   Future<void> hubCommander(String json) async {
@@ -156,8 +157,9 @@ class NewServerFormState extends ParentFormState {
             return;
           } else if (key == 'dump_and_clear_log') {
             // return log entries, empty the buffer
-            hubWrite({'log': global.logMan.buffer.toString()});
-            global.logMan.buffer.clear();
+            var manager = logm.LoggerManager();
+            hubWrite({'log': manager.buffer.toString()});
+            manager.buffer.clear();
             return;
           } else if (key == 'exit') {
             // done with commands--close TCP connection
@@ -196,8 +198,8 @@ class NewServerFormState extends ParentFormState {
   }
 
   static Future<String> bbProxy({toAddress, toPort}) async {
-    final hub = global.loginState.hub;
-    final lk = global.loginState.pureLoginKey;
+    final hub = loginState.hub;
+    final lk = loginState.pureLoginKey;
     final url = 'wss://$hub:8443/v1/managers/$lk/servers/18/proxy';
     io.WebSocket.connect(url).then((io.WebSocket ws) async {
       try {

app/lib/parent_form_state.dart
@@ -3,10 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logging/logging.dart';
-import 'global.dart' as global;
 import 'main.dart';
 
 final _log = Logger('parent_form_state');
+var loginState = LoginState.instance;
 
 const String base28Digits = '23456789BCDFGHJKLMNPQRSTVWXZ';
 const int accountLen = 21; // including dashes
@@ -220,8 +220,8 @@ abstract class ParentFormState extends State<ParentForm> with RestorationMixin {
       }
     }
     if (error.isEmpty) {
-      if (global.loginState.hub != hub) {
-        error = "B99034 '${global.loginState.hub}'!='$hub'";
+      if (loginState.hub != hub) {
+        error = "B99034 '${loginState.hub}'!='$hub'";
       }
     }
     if (error.isEmpty) {

app/lib/servers_screen.dart
@@ -7,11 +7,11 @@ import 'package:go_router/go_router.dart';
 import 'package:logging/logging.dart';
 import 'dart:math';
 import 'dart:convert' as convert;
-import 'global.dart' as global;
 import 'main.dart';
 import 'parent_form_state.dart';
 
 final _log = Logger('servers_screen');
+var loginState = LoginState.instance;
 
 class ServersScreen extends StatelessWidget {
   const ServersScreen({Key? key}) : super(key: key);
@@ -33,8 +33,8 @@ class ServersFormState extends ParentFormState {
 
   @override
   Future<http.Response?> callApi() {
-    String domain = '${global.loginState.hub}:8443';
-    String path = '/v1/managers/${global.loginState.pureLoginKey}/servers';
+    String domain = '${loginState.hub}:8443';
+    String path = '/v1/managers/${loginState.pureLoginKey}/servers';
     _log.info("POST https $domain$path");
     return http.post(Uri.https(domain, path));
   }
@@ -60,11 +60,11 @@ class ServersFormState extends ParentFormState {
   nextScreen() => context.push('/new-server');
 
   @override
-  String getHubValue() => global.loginState.hub;
+  String getHubValue() => loginState.hub;
 
   @override
   void setHubValue(value) {
-    global.loginState.hub = value;
+    loginState.hub = value;
   }
 
   @override
@@ -72,7 +72,7 @@ class ServersFormState extends ParentFormState {
 
   @override
   void setAccountValue(value) {
-    global.loginState.loginKey = value;
+    loginState.loginKey = value;
   }
 
   @override
@@ -88,7 +88,7 @@ class ServersFormState extends ParentFormState {
             sizedBoxSpace,
             FractionallySizedBox(
               widthFactor: 0.8,
-              child: global.loginState.isNotSignedIn()
+              child: loginState.isNotSignedIn()
                   ? Text("You need to sign in.",
                       textAlign: TextAlign.center,
                       textScaleFactor: 1.8,
@@ -96,7 +96,7 @@ class ServersFormState extends ParentFormState {
                         fontStyle: FontStyle.italic,
                         color: Theme.of(context).colorScheme.background,
                       ))
-                  : global.loginState.servers.isEmpty
+                  : loginState.servers.isEmpty
                       ? Text("You have no VPN servers set up.",
                           textAlign: TextAlign.center,
                           textScaleFactor: 1.8,
@@ -117,7 +117,7 @@ class ServersFormState extends ParentFormState {
                 child: SizedBox(
                   width: min(MediaQuery.of(context).size.width, 700),
                   child: ListView.builder(
-                    itemCount: global.loginState.servers.length,
+                    itemCount: loginState.servers.length,
                     padding: const EdgeInsets.symmetric(horizontal: 18),
                     itemBuilder: (context, index) =>
                         vpnServerCard(context, index),
@@ -157,7 +157,7 @@ class ServersFormState extends ParentFormState {
                       width: 42,
                     ),
                     title: Text(
-                      "VPN server ${global.loginState.servers[index]['id']}",
+                      "VPN server ${loginState.servers[index]['id']}",
                       style: const TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,

app/lib/sign_in_screen.dart
@@ -6,11 +6,11 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart' as storage;
 import 'package:logging/logging.dart';
 import 'dart:convert' as convert;
 import 'dart:math';
-import 'global.dart' as global;
 import 'main.dart';
 import 'parent_form_state.dart';
 
 final _log = Logger('sign_in_screen');
+var loginState = LoginState.instance;
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -43,8 +43,8 @@ class SignInFormState extends ParentFormState {
 
   @override
   Future<http.Response?> callApi() {
-    String domain = '${global.loginState.hub}:8443';
-    String path = '/v1/managers/${global.loginState.pureLoginKey}/servers';
+    String domain = '${loginState.hub}:8443';
+    String path = '/v1/managers/${loginState.pureLoginKey}/servers';
     _log.info("GET https $domain$path");
     return http.get(Uri.https(domain, path));
   }
@@ -52,16 +52,16 @@ class SignInFormState extends ParentFormState {
   @override
   String statusCodeCheck(status) {
     var displayError = statusCodeMessage(status, item: "login key");
-    global.loginState.loginKeyVerified = displayError.isEmpty;
+    loginState.loginKeyVerified = displayError.isEmpty;
     const keyStore = storage.FlutterSecureStorage();
     // if loginState.saveLoginKey == false, values have already been cleared
-    if (global.loginState.saveLoginKey && global.loginState.loginKeyVerified) {
+    if (loginState.saveLoginKey && loginState.loginKeyVerified) {
       // only save login key if user opts in AND login key is valid
-      _log.info("Save to secure storage: hub ${global.loginState.hub}");
-      keyStore.write(key: 'hub', value: global.loginState.hub);
+      _log.info("Save to secure storage: hub ${loginState.hub}");
+      keyStore.write(key: 'hub', value: loginState.hub);
       _log.info("Save to secure storage: "
-          "login key ${global.loginState.loginKey}");
-      keyStore.write(key: 'login_key', value: global.loginState.loginKey);
+          "login key ${loginState.loginKey}");
+      keyStore.write(key: 'login_key', value: loginState.loginKey);
       _log.info("Save to secure storage: verification state 'true'");
       keyStore.write(key: 'login_key_verified', value: 'true');
     }
@@ -70,8 +70,7 @@ class SignInFormState extends ParentFormState {
 
   @override
   String processApiResponse(response) {
-    global.loginState.servers =
-        convert.jsonDecode(response.body) as List<dynamic>;
+    loginState.servers = convert.jsonDecode(response.body) as List<dynamic>;
     return "";
   }
 
@@ -79,19 +78,19 @@ class SignInFormState extends ParentFormState {
   nextScreen() => context.push('/servers');
 
   @override
-  String getHubValue() => global.loginState.hub;
+  String getHubValue() => loginState.hub;
 
   @override
   void setHubValue(value) {
-    global.loginState.hub = value;
+    loginState.hub = value;
   }
 
   @override
-  String getAccountValue() => global.loginState.loginKey;
+  String getAccountValue() => loginState.loginKey;
 
   @override
   void setAccountValue(value) {
-    global.loginState.loginKey = value;
+    loginState.loginKey = value;
   }
 
   @override
@@ -161,10 +160,10 @@ class SignInFormState extends ParentFormState {
                           controlAffinity: ListTileControlAffinity.leading,
                           contentPadding: EdgeInsets.zero,
                           dense: true,
-                          value: global.loginState.saveLoginKey,
+                          value: loginState.saveLoginKey,
                           onChanged: (value) {
                             setState(() {
-                              global.loginState.saveLoginKey = value == true;
+                              loginState.saveLoginKey = value == true;
                             });
                           },
                         ),
@@ -197,7 +196,7 @@ class SignInFormState extends ParentFormState {
   }
 
   void signIn() {
-    if (global.loginState.saveLoginKey == false) {
+    if (loginState.saveLoginKey == false) {
       // if box not checked, clear stored login key even before trying server
       clearStoredLoginKey();
     }
@@ -215,14 +214,14 @@ class SignInFormState extends ParentFormState {
       // going to a different screen and then to '/sign-in' is the only reliable way
       // ... I could find to clear fields if user is already on SignInScreen
       SignInFormState.clearStoredLoginKey();
-      global.loginState.saveLoginKey = false;
-      global.loginState.loginKey = '';
-      global.loginState.loginKeyVerified = false;
+      loginState.saveLoginKey = false;
+      loginState.loginKey = '';
+      loginState.loginKeyVerified = false;
       GoRouter.of(context).go('/sign-in');
     }
     // if login key is verified, press the sign-in button (virtually); this
     // ... is similar to a redirect but if login fails will stay on this screen
-    if (global.loginState.loginKeyVerified) {
+    if (loginState.loginKeyVerified) {
       signIn();
     }
   }

app/lib/welcome_screen.dart
@@ -8,12 +8,12 @@ import 'package:json_rpc_2/json_rpc_2.dart' as jsonrpc;
 import 'package:logging/logging.dart';
 import 'dart:convert' as convert;
 import 'dart:math';
-import 'global.dart' as global;
 import 'main.dart';
 import 'parent_form_state.dart';
 import 'persistent_websocket.dart';
 
 final _log = Logger('welcome_screen');
+var loginState = LoginState.instance;
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -43,9 +43,9 @@ class WelcomeFormState extends ParentFormState {
     try {
       var url = Uri(
               scheme: 'wss',
-              host: global.loginState.hub,
+              host: loginState.hub,
               port: 8443,
-              path: '/rpc1/${global.loginState.pureCoupon}/4')
+              path: '/rpc1/${loginState.pureCoupon}/4')
           .toString();
       _log.info("connecting to $url");
       final hubMessages = PersistentWebSocket('');
@@ -62,8 +62,8 @@ class WelcomeFormState extends ParentFormState {
     } catch (err) {
       _log.warning("B40125 pws: $err");
     }
-    String domain = '${global.loginState.hub}:8443';
-    String path = '/v1/coupons/${global.loginState.pureCoupon}/managers';
+    String domain = '${loginState.hub}:8443';
+    String path = '/v1/coupons/${loginState.pureCoupon}/managers';
     _log.info("POST https $domain$path");
     return http.post(Uri.https(domain, path));
   }
@@ -85,10 +85,9 @@ class WelcomeFormState extends ParentFormState {
     if (pureLoginKey == null || pureLoginKey.length != accountLen - 3) {
       return "login_key is $pureLoginKey"; // error
     }
-    global.loginState.newLoginKey =
-        global.loginState.dressLoginKey(pureLoginKey);
-    global.loginState.loginKey = ''; // force user to type it
-    global.loginState.loginKeyVerified = false;
+    loginState.newLoginKey = loginState.dressLoginKey(pureLoginKey);
+    loginState.loginKey = ''; // force user to type it
+    loginState.loginKeyVerified = false;
     return "";
   }
 
@@ -96,11 +95,11 @@ class WelcomeFormState extends ParentFormState {
   nextScreen() => context.push('/new-login-key');
 
   @override
-  String getHubValue() => global.loginState.hub;
+  String getHubValue() => loginState.hub;
 
   @override
   void setHubValue(value) {
-    global.loginState.hub = value;
+    loginState.hub = value;
   }
 
   @override
@@ -108,7 +107,7 @@ class WelcomeFormState extends ParentFormState {
 
   @override
   void setAccountValue(value) {
-    global.loginState.coupon = value;
+    loginState.coupon = value;
   }
 
   @override

commit a44e9fcef1ce9dd2633ff524d388c5fb87a50014

Fix log output (broken in 106d1cd)

app/lib/main.dart
@@ -19,6 +19,7 @@ var loginState = LoginState.instance;
 
 void main() async {
   usePathUrlStrategy(); // turn off the extra `#/` in the URLs
+  LoggerManager();
   _log.info("Begin Bitburrow app");
   WidgetsFlutterBinding.ensureInitialized();
   await onAppStart();