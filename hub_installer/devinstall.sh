9572a8e535cd70328fdc14b8f1379948e87716bc
bitinerant <bitinerant@users.noreply.github.com>
2023-12-07 10:38:14 +0100

    Add devinstall.sh to simplify development testing for the hub

---
+++ hub_installer/devinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e # exit script if anything fails
+
+## how to use a BitBurrow hub for development testing
+
+#
+# 1. clone the repo:
+#        mkdir "where_you_keep_code/bitburrow" && cd "$_"
+#        git clone https://github.com/BitBurrow/BitBurrow.git .
+# 2. make changes
+# 3. stop BitBurrow hub service (as user ubuntu or root):
+#        sudo systemctl stop bitburrow
+# 4. copy your modified files to ~/dev/hub via something similar to:
+#        rsync -av --files-from <(git ls-files; git ls-files --others --exclude-standard) \
+#            ./ bitburrow@your_hub:dev/hub/
+# 5. run this script:
+#        bash dev/hub/hub_installer/devinstall.sh
+# 6. run BitBurrow hub in debug mode:
+#        .local/bin/bbhub --daemon -vv
+#
+
+## download BitBurrow hub dependencies
+
+cd ~/
+# if last updated 47+ hours ago or never, or dependencies have changed
+if ! [ -f dev/dependencies/pyproject.toml.md5sum ] \
+        || find dev/dependencies/pyproject.toml.md5sum -mmin +2820 |grep -q last_updated \
+        || ! md5sum --check --status dev/dependencies/pyproject.toml.md5sum; then
+    echo ======= downloading BitBurrow hub dependencies =======
+    python3 -m pip download dev/hub/ poetry-core --dest dev/dependencies/
+    md5sum dev/hub/pyproject.toml >dev/dependencies/pyproject.toml.md5sum
+fi
+
+## install BitBurrow hub from ~/dev/hub/ and local download cache
+
+echo ======= pip-installing BitBurrow hub =======
+python3 -m pip install dev/hub/ --no-index --find-links dev/dependencies/
+
+
