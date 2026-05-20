BitBurrow/BitBurrow

commit 20b5ccb610d4529d9b2acca65dbdfee755010c1b

Clean up database table definitions

hub/db.py
@@ -17,7 +17,7 @@ from sqlmodel import Field, Session, SQLModel, select, JSON, Column, Relationshi
 import subprocess
 import tempfile
 import threading
-from typing import Optional, Any
+from typing import Any
 import urllib.parse
 import hub.login_key as lk
 import hub.net as net
@@ -112,7 +112,7 @@ lkocc_string = '__login_key_or_coupon_code__'
 
 
 class Account(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, default=None)
+    id: int | None = Field(primary_key=True, default=None)
     login: str = Field(index=True, unique=True)  # 'login' field is used like a username
     key_hash: str = ''  # Argon2 hash of key (used like a hashed password)
     clients_max: int = 7
@@ -122,12 +122,12 @@ class Account(SQLModel, table=True):
     )
     valid_until: DateTime = Field(
         sa_column=Column(sqlalchemy.DateTime(timezone=True)),
-        default=DateTime(1970, 1, 1, tzinfo=TimeZone.utc),  # Unix epoc
+        default=DateTime(1970, 1, 1, tzinfo=TimeZone.utc),  # Unix epoch
     )
     kind: AccountKind = AccountKind.NONE
-    parent_id: int = 0  # id of Account that created this one, e.g. coupon code
-    intf_id: Optional[int] = Field(foreign_key='intf.id')  # used only if kind == USER
-    email: str = ""  # optional, allow login key reset
+    parent_id: int | None = Field(foreign_key='account.id', default=None)  # this account's creator
+    intf_id: int | None = Field(foreign_key='intf.id', default=None)  # used only if kind == USER
+    email: str = ''  # optional, allow login key reset
     comment: str = ""
     login_sessions: list['LoginSession'] = Relationship(back_populates='account')
     devices: list['Device'] = Relationship(back_populates='account')
@@ -299,7 +299,7 @@ class LoginSessionKind(enum.Enum):
 
 
 class LoginSession(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, default=None)
+    id: int | None = Field(primary_key=True, default=None)
     account_id: int = Field(foreign_key='account.id')
     kind: LoginSessionKind = LoginSessionKind.DISABLED
     token_hash: str = Field(index=True, unique=True)
@@ -315,9 +315,9 @@ class LoginSession(SQLModel, table=True):
         sa_column=Column(sqlalchemy.DateTime(timezone=True), index=True),
         default_factory=lambda: DateTime.now(TimeZone.utc) + TimeDelta(days=1),
     )
-    account: Optional[Account] = Relationship(back_populates="login_sessions")
-    ip: str
-    user_agent: str
+    account: Account | None = Relationship(back_populates='login_sessions')
+    ip: str = ''
+    user_agent: str = ''
 
 
 def new_login_session(
@@ -379,20 +379,20 @@ def iter_get_login_session_by_account_id(aid: int | None):
 
 class Device(SQLModel, table=True):
     __table_args__ = (  # name_slug must be unique *for this user*
-        sqlalchemy.UniqueConstraint("account_id", "name_slug", name="uq_device_account_slug"),
-        sqlalchemy.Index("ix_device_account_slug", "account_id", "name_slug"),
+        sqlalchemy.UniqueConstraint('account_id', 'name_slug', name='uq_device_account_slug'),
+        sqlalchemy.Index('ix_device_account_slug', 'account_id', 'name_slug'),
     )
-    id: Optional[int] = Field(primary_key=True, default=None)
-    account_id: Optional[int] = Field(index=True, foreign_key='account.id')  # device admin--manager
+    id: int | None = Field(primary_key=True, default=None)
+    account_id: int = Field(index=True, foreign_key='account.id')  # device admin--manager
     name: str = ''  # e.g. "Base SPW" but user can modify
-    name_slug: Optional[str] = Field(index=True)  # URL-safe version of name
+    name_slug: str = Field(index=True)  # URL-safe version of name
     # for bases, subd is the left-most label of FQDN, e.g. y99g in y99g.vxm.example.org
-    subd: Optional[str] = Field(index=True, unique=True)
+    subd: str = Field(index=True, unique=True)
     last_endpoint: str | None = None  # most recent public IP
     last_handshake: int | None = None  # seconds past Unix epoch
-    ott_id: Optional[int] = Field(foreign_key='loginsession.id')  # one-time token for adopting
+    ott_id: int | None = Field(foreign_key='loginsession.id', default=None)  # one-time token
     auth_pubkey: str = ''  # pubkey for API auth after initial auth via ott_id
-    account: Optional[Account] = Relationship(back_populates="devices")
+    account: Account | None = Relationship(back_populates='devices')
     comment: str = ""
 
     def adopt_state(self) -> str:
@@ -443,8 +443,8 @@ class IntfMethod(enum.Enum):  # method used to configure Wireguard
 
 
 class Intf(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, default=None)
-    device_id: Optional[int] = Field(index=True, foreign_key='device.id')  # device this intf is on
+    id: int | None = Field(primary_key=True, default=None)
+    device_id: int = Field(index=True, foreign_key='device.id')  # device this intf is on
     ipv4_base: str = ''  # ipaddress.ip_network() but without the subnet prefix
     ipv6_base: str = ''
     host_id: int = 0  # host portion of IP address, e.g. 1 for muti-peer; applies to IPv4 and IPv6
@@ -452,19 +452,19 @@ class Intf(SQLModel, table=True):
     allowed_ipv4_subnet: int = 32  # our allowed IPs, i.e. AllowedIPs in Peer section of our peer
     allowed_ipv6_subnet: int = 128
     # 'base_intf_id' is our peer (server) on single-peer interfaces; otherwise None
-    base_intf_id: Optional[int] = Field(index=True, foreign_key='intf.id')
+    base_intf_id: int | None = Field(index=True, foreign_key='intf.id', default=None)
     wg_privkey: str = Field(index=True, unique=True, nullable=False)
     wg_pubkey: str = Field(index=True, unique=True, nullable=False)
-    backend_port: int | None
+    backend_port: int | None = None
     # use JSON because lists are not yet supported: https://github.com/tiangolo/sqlmodel/issues/178
-    frontend_ports: list[int] = Field(sa_column=Column(JSON))  # on base's public IP
+    frontend_ports: list[int] = Field(sa_column=Column(JSON), default_factory=list)  # on public IP
     # 'other' is a dict of all other config options, official and custom
-    keepalive: int | None
+    keepalive: int | None = None
     other: dict[str, Any] = Field(sa_column=Column(JSON), default_factory=dict)
-    last_endpoint: str | None
-    last_handshake: int | None  # seconds past Unix epoch
-    ssh_privkey: str | None
-    ssh_pubkey: str | None
+    last_endpoint: str | None = None
+    last_handshake: int | None = None  # seconds past Unix epoch
+    ssh_privkey: str | None = None
+    ssh_pubkey: str | None = None
     comment: str = ""
     default_method: IntfMethod = IntfMethod.NONE
     model_config = ConfigDict(arbitrary_types_allowed=True)  # for Column(JSON)

versions
@@ -1,3 +1,3 @@
 hub/bbbased.lua	2026-05-01T08:42:41Z
 hub/adopt5p.sh	2026-05-01T08:48:47Z
-hub/	2026-05-02T11:01:37Z
+hub/	2026-05-05T19:36:01Z

commit 669809304d54dd903e0c2ca5add013549afa6cdf

Fix 3 database 'NOT NULL' bugs introduced in 20b5ccb

hub/db.py
@@ -383,11 +383,11 @@ class Device(SQLModel, table=True):
         sqlalchemy.Index('ix_device_account_slug', 'account_id', 'name_slug'),
     )
     id: int | None = Field(primary_key=True, default=None)
-    account_id: int = Field(index=True, foreign_key='account.id')  # device admin--manager
+    account_id: int | None = Field(index=True, foreign_key='account.id')  # device admin--manager
     name: str = ''  # e.g. "Base SPW" but user can modify
-    name_slug: str = Field(index=True)  # URL-safe version of name
+    name_slug: str = Field(index=True, default='')  # URL-safe version of name
     # for bases, subd is the left-most label of FQDN, e.g. y99g in y99g.vxm.example.org
-    subd: str = Field(index=True, unique=True)
+    subd: str = Field(index=True, unique=True, default='')
     last_endpoint: str | None = None  # most recent public IP
     last_handshake: int | None = None  # seconds past Unix epoch
     ott_id: int | None = Field(foreign_key='loginsession.id', default=None)  # one-time token
@@ -1207,11 +1207,19 @@ def hub_intf_exists(session):
     return session.exec(select(Intf).where(Intf.id == hub_id)).first() is not None
 
 
+def new_device_subd(session, device: Device) -> None:
+    # a domain name that begins with a digit is legal but occasionally problematic
+    # up to 439,040 base routers; Linux probably can't handle nearly that many anyhow
+    subd = lambda _: (lk.generate_login_key_letters(1) + lk.generate_login_key(3)).lower()
+    set_unique_value(session, device, 'subd', retry=200, valuef=subd)
+
+
 def create_hub_if_missing(session):
     """Don't call before needed in case conf.get('frontend.wg_port') is changed."""
     if not hub_intf_exists(session):
         # create the hub Device
         hub_device = Device(account_id=None, name="BitBurrow hub")
+        new_device_subd(session, hub_device)
         session.add(hub_device)
         session.commit()
         assert hub_device.id == hub_id, f"B12466 unexpected {hub_device.id=}"
@@ -1229,13 +1237,7 @@ def new_device(account_id, is_base: bool, name: str) -> str:
         if is_base:
             create_hub_if_missing(session)  # in case this is the first base router
         device = Device(account_id=account_id)
-        if is_base:
-            # a domain name that begins with a digit is legal but occasionally problematic
-            # up to 439,040 base routers; Linux probably can't handle nearly that many anyhow
-            subd = lambda _: (lk.generate_login_key_letters(1) + lk.generate_login_key(3)).lower()
-            set_unique_value(session, device, 'subd', retry=200, valuef=subd)
-        else:
-            device.subd = None
+        new_device_subd(session, device)
         device.name = name
         slugged = util.slugify(device.name)
         # find a unique (for this user) name_slug

versions
@@ -1,2 +1,2 @@
 0tf4ijm hub/bbbased.lua
-0tf4ijm hub/
+0tfctdk hub/